ca767fe4e803c2a6593571c26c52bdd9c1132d5d
mayur <mayurpatkar68@gmail.com>
2024-02-09 21:50:43 +0530

    update 0

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-    menu.innerHTML = `<li><a href="#Education" onclick="toggleMenu()">Education</a></li>
-            <li><a href="#Skills" onclick="toggleMenu()">Skills</a></li>
-            <li><a href="#projects" onclick="toggleMenu()">Projects</a></li>
-            <li><a href="#Achievements" onclick="toggleMenu()">Achievements</a></li>
-            <li><a href="#contact" onclick="toggleMenu()">Contact</a></li>
-            <div class="moblie-toggle-mode">
-                <a>Dark</a>
-                <i class="bi bi-brightness-high-fill" id="moblie-toggle-dark"></i>
-            </div>`;
 }
 
 // Function to handle toggling classes
@@ -36,33 +27,18 @@ menuToggle.addEventListener('click', function () {
 });
 
 // Second event listener
-// const menuToggle_ = document.getElementById('moblie-toggle-dark');
-// const shadowOverlay_ = document.querySelector('.shadow-overlay');
+const menuToggle_ = document.getElementById('moblie-toggle-dark');
+const shadowOverlay_ = document.querySelector('.shadow-overlay');
 
-// menuToggle_.addEventListener('click', function () {
-//     toggleClasses(this, shadowOverlay_);
-// });
-
-
-function github() {
-    window.open("https://github.com/Mayur68", "_blank");
-}
-
-function linkedin() {
-    window.open("https://www.linkedin.com/in/mayura-patkar-14306a2b1/", "_blank");
-}
-
-window.addEventListener('scroll', function () {
-    var introText = document.querySelector('.intro-text');
-    var aboutText = document.querySelector('.about-text');
-    var introPosition = introText.getBoundingClientRect().top;
-    var aboutPosition = aboutText.getBoundingClientRect().top;
-
-    if (introPosition <= 0) {
-        introText.style.top = '-100%';
-        aboutText.style.top = '0';
-    } else {
-        introText.style.top = '0';
-        aboutText.style.top = '100vh';
-    }
+menuToggle_.addEventListener('click', function () {
+    toggleClasses(this, shadowOverlay_);
 });
+
+
+// function github() {
+//     window.open("https://github.com/Mayur68", "_blank");
+// }
+
+// function linkedin() {
+//     window.open("https://www.linkedin.com/in/mayura-patkar-14306a2b1/", "_blank");
+// }
