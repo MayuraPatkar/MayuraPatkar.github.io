019c71c73d3308f459948aed6d980a179af507f3
mayur <mayurpatkar68@gmail.com>
2024-02-10 22:14:33 +0530

    update 7

--- script.js
+++ script.js
@@ -27,12 +27,33 @@ function toggleClasses(toggleElement, ...elements) {
 const menuToggle = document.getElementById('toggle-dark');
 const shadowOverlay = document.querySelector('.shadow-overlay');
 const linkedin = document.querySelector('.linkedin-dark-mode');
+const line = document.querySelector('.line');
 const linkedin2 = document.querySelector('.linkedin2-dark-mode');
 const github = document.querySelector('.github-dark-mode');
 const email = document.querySelector('.email-dark-mode');
 const x = document.querySelector('.x-dark-mode');
 
 menuToggle.addEventListener('click', function () {
-    toggleClasses(this, document.body, shadowOverlay, linkedin,linkedin2, github, email, x);
+    toggleClasses(this, document.body, shadowOverlay, line, linkedin, linkedin2, github, email, x);
 });
 
+
+function go_up() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function toggleGoUpButton() {
+    var goUpButton = document.querySelector(".go-up");
+    if (window.scrollY > 0) {
+        goUpButton.classList.add("visible");
+    } else {
+        goUpButton.classList.remove("visible");
+    }
+}
+
+window.addEventListener("scroll", toggleGoUpButton);
+
+toggleGoUpButton();
